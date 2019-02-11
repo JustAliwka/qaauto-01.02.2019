ef51abf15d9a3b0d7d695b96612e30c8f04d0816
Alina Panchenko <panalina3@gmail.com>
2019-02-11 11:12:22 +0200

    Finished NegativeLogin test

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,21 +10,24 @@ public class LoginTests {
     @Test
     public void NegativeLoginTest() {
 
-        String UserName = "panalina";
-        String UserPassword = "something";
+        String UserName = "a@b.c";
+        String UserPassword = "";
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com");
-        //LinkedIn: Войти или зарегистрироваться
-        Assert.assertEquals(driver.getTitle(),"LinkedIn: Войти или зарегистрироваться", "Login page title is wrong");
+
+        Assert.assertEquals(driver.getTitle(),"LinkedIn: Log In or Sign Up", "Login page title is wrong");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/", "Page URL is not correct");
+        Assert.assertTrue(driver.findElement(By.xpath("//input[@id='login-submit']")).isDisplayed());
+
         WebElement UserNameField = driver.findElement(By.xpath("//input[@id='login-email']"));
         WebElement UserPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
         WebElement SubmitButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
         UserNameField.sendKeys(UserName);
         UserPasswordField.sendKeys(UserPassword);
-        SubmitButton.sendKeys(Keys.ENTER);
+        SubmitButton.click();
 
-
+        Assert.assertEquals(driver.getTitle(),"LinkedIn: Log In or Sign Up", "Login page title is wrong");
 
         driver.quit();
     }
